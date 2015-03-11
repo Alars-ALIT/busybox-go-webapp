5839cfdf105d57b2abbe578453b157306fe40639
Andreas Larsson <alarsson@williamsinteractive.com>
2015-03-11 12:31:37 +0100

    test

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ MAINTAINER Andreas Larsson
 ADD ./release/go-app /bin/go-app
 RUN chmod 755 /bin/go-app
 
+EXPOSE 8080
+
 ENTRYPOINT ["/bin/go-app"]
--- server.go
+++ server.go
@@ -6,12 +6,12 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi, I like %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "ROBOT6, I fisti %s!", r.URL.Path[1:])
 	fmt.Println("Received request: s%", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Sttttarted app at :8080")
+	fmt.Println("ROBOT6 i  i i  gulli :8080")
 	http.ListenAndServe(":8080", nil)
 }
